d77be19d9f4b25c9fa0bd9dfbf44c1886dd6712b
Leonard Isaac <95531716+leoimewore@users.noreply.github.com>
2022-04-25 20:37:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,11 @@
 const answer = document.querySelectorAll(".answer");
 const arrow = document.querySelectorAll(".arr");
 const question = document.getElementsByTagName("h2");
+const paragraghColor = document.querySelectorAll('.para')
 //Initial Click
 for (let i = 0; i < question.length; i++)
   question[i].addEventListener("click", function () {
-    question[i].classList.toggle('colour')
+    question[i].style.color ='black'
     answer[i].classList.toggle("hidden");
     arrow[i].classList.toggle("rotate");
   });
@@ -15,5 +16,8 @@ for (let i = 0; i < question.length; i++)
 question.addEventListener("click", function () {
   arrow[i].classList.toggle("rotate");
   answer[i].classList.toggle("hidden");
+  
+  
  
 });
+
